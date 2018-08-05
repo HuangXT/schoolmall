fff29155b83e27362d93df842898780b353e563c
Lucky <641787296@qq.com>
2018-08-05 14:44:17 +0800

    初始化校园购空间V1.0.4 1.主要SpringCloud组件-服务注册中心eurekaServer

---
+++ cloud-base-server/cloud-registration-center/src/main/java/com/schoolmall/xc/micro/server/eureka/EurekaServerApplication.java
@@ -0,0 +1,25 @@
+package com.schoolmall.xc.micro.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Lucky
+ * @Description: 主要SpringCloud组件-服务注册中心eurekaServer
+ * @Date: Created in 12:45 2018/8/5
+ * @Modified By:
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
